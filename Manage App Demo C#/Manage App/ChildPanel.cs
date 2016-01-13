79f0b3822272b840461f3e988cacc367b04d748f
haminhcong <haminhcongbkhn@gmail.com>
2016-01-13 20:44:37 +0700

    add System demo by C# program

---
+++ Manage App Demo C#/Manage App/ChildPanel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manage_App
+{
+    public partial class ChildPanel : Form
+    {
+        private MainForm parentForm;
+        public ChildPanel()
+        {
+        }
+        public ChildPanel(MainForm parentForm)
+        {
+            this.parentForm = parentForm;
+            InitializeComponent();
+            configPanel();
+        }
+
+        private void configPanel()
+        {
+            this.MdiParent = parentForm;
+            //this.Size = parentForm.getMainPanelSize();
+            //this.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Top);
+            this.Dock = DockStyle.Fill;
+            this.WindowState = FormWindowState.Normal;
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            //this.TopLevel = false;
+        }
+
+    }
+}
---
+++ Manage App Demo C#/Manage App/LoginForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manage_App
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+          /* DialogResult confirm = MessageBox.Show("Do you want to exit ?", "Exit", MessageBoxButtons.YesNo);
+            if (confirm == DialogResult.Yes)
+            {
+            }
+            else
+            {
+                e.Cancel = true;
+            }*/
+        }
+
+        private void LoginForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //System.Windows.Forms.Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm mainForm = new MainForm();
+            //ChildPanel mainForm = new ChildPanel();
+            this.Hide();
+            mainForm.Show();
+        }
+    }
+}
